473f298a0a27ba14c7ba2ba22fccd2bcf0f33c8a
Gunjan297 <mgpsgunjan6166@gmail.com>
2025-07-05 11:01:24 +0530

    Update constants.js to connect to deployed backend

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,9 +1,9 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const USER_API_END_POINT = "https://bridge-aid-backend.onrender.com/api/v1/user";
 
-export const SCHEMES_API_END_POINT = "http://localhost:8000/api/v1/scheme";
+export const SCHEMES_API_END_POINT = "https://bridge-aid-backend.onrender.com/api/v1/scheme";
 
-export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
+export const APPLICATION_API_END_POINT = "https://bridge-aid-backend.onrender.com/api/v1/application";
 
-export const ORGANIZATION_API_END_POINT = "http://localhost:8000/api/v1/org";
+export const ORGANIZATION_API_END_POINT = "https://bridge-aid-backend.onrender.com/api/v1/org";
 
 
